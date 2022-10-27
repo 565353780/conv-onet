20bac347ec9e43c0882fbe3efde7cfcde74668c4
chLi <565353780@qq.com>
2022-10-27 17:10:35 +0800

    add basic trainer class

---
+++ conv_onet/Demo/trainer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from conv_onet.Module.trainer import Trainer
+
+def demo():
+    trainer = Trainer()
+    return True
---
+++ conv_onet/Module/trainer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class Trainer(object):
+
+    def __init__(self):
+        return
