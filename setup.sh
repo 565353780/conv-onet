23bbc67b5bee384026b249b0cfc8e97f4dbef68d
chLi <565353780@qq.com>
2022-10-27 17:10:42 +0800

    update setup bash files

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 pip install cython imageio numpy matplotlib pandas pillow pyembree pytest pyyaml \
-  scikit-image scipy tensorboardx tqdm trimesh h5py plyfile torch_scatter
+  scikit-image scipy tensorboardx tqdm trimesh h5py plyfile
 
 pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
+
+pip install torch-scatter -f https://data.pyg.org/whl/torch-1.12.0+cu113.html
+
+python setup.py build_ext --inplace
 
