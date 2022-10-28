094f131b397f59ab7fa215a1fe14ac184f49d779
chLi <565353780@qq.com>
2022-10-28 20:18:45 +0800

    add binvoxel class and method

---
+++ conv_onet/Method/voxels.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+
+def read_header(fp):
+    """ Read binvox header. Mostly meant for internal use.
+    """
+    line = fp.readline().strip()
+    if not line.startswith(b'#binvox'):
+        raise IOError('Not a binvox file')
+    dims = [int(i) for i in fp.readline().strip().split(b' ')[1:]]
+    translate = [float(i) for i in fp.readline().strip().split(b' ')[1:]]
+    scale = [float(i) for i in fp.readline().strip().split(b' ')[1:]][0]
+    line = fp.readline()
+    return dims, translate, scale
+
+
+def read_as_3d_array(fp, fix_coords=True):
+    """ Read binary binvox format as array.
+
+    Returns the model with accompanying metadata.
+
+    Voxels are stored in a three-dimensional numpy array, which is simple and
+    direct, but may use a lot of memory for large models. (Storage requirements
+    are 8*(d^3) bytes, where d is the dimensions of the binvox model. Numpy
+    boolean arrays use a byte per element).
+
+    Doesn't do any checks on input except for the '#binvox' line.
+    """
+    dims, translate, scale = read_header(fp)
+    raw_data = np.frombuffer(fp.read(), dtype=np.uint8)
+    # if just using reshape() on the raw data:
+    # indexing the array as array[i,j,k], the indices map into the
+    # coords as:
+    # i -> x
+    # j -> z
+    # k -> y
+    # if fix_coords is true, then data is rearranged so that
+    # mapping is
+    # i -> x
+    # j -> y
+    # k -> z
+    values, counts = raw_data[::2], raw_data[1::2]
+    data = np.repeat(values, counts).astype(np.bool)
+    data = data.reshape(dims)
+    if fix_coords:
+        # xzy to xyz TODO the right thing
+        data = np.transpose(data, (0, 2, 1))
+        axis_order = 'xyz'
+    else:
+        axis_order = 'xzy'
+    return data, dims, translate, scale, axis_order
+
+
+def read_as_coord_array(fp, fix_coords=True):
+    """ Read binary binvox format as coordinates.
+
+    Returns binvox model with voxels in a "coordinate" representation, i.e.  an
+    3 x N array where N is the number of nonzero voxels. Each column
+    corresponds to a nonzero voxel and the 3 rows are the (x, z, y) coordinates
+    of the voxel.  (The odd ordering is due to the way binvox format lays out
+    data).  Note that coordinates refer to the binvox voxels, without any
+    scaling or translation.
+
+    Use this to save memory if your model is very sparse (mostly empty).
+
+    Doesn't do any checks on input except for the '#binvox' line.
+    """
+    dims, translate, scale = read_header(fp)
+    raw_data = np.frombuffer(fp.read(), dtype=np.uint8)
+
+    values, counts = raw_data[::2], raw_data[1::2]
+
+    sz = np.prod(dims)
+    index, end_index = 0, 0
+    end_indices = np.cumsum(counts)
+    indices = np.concatenate(([0], end_indices[:-1])).astype(end_indices.dtype)
+
+    values = values.astype(np.bool)
+    indices = indices[values]
+    end_indices = end_indices[values]
+
+    nz_voxels = []
+    for index, end_index in zip(indices, end_indices):
+        nz_voxels.extend(range(index, end_index))
+    nz_voxels = np.array(nz_voxels)
+    # TODO are these dims correct?
+    # according to docs,
+    # index = x * wxh + z * width + y; // wxh = width * height = d * d
+
+    x = nz_voxels / (dims[0] * dims[1])
+    zwpy = nz_voxels % (dims[0] * dims[1])  # z*w + y
+    z = zwpy / dims[0]
+    y = zwpy % dims[0]
+    if fix_coords:
+        data = np.vstack((x, y, z))
+        axis_order = 'xyz'
+    else:
+        data = np.vstack((x, z, y))
+        axis_order = 'xzy'
+
+    # return data, dims, translate, scale, axis_order
+    return np.ascontiguousarray(data), dims, translate, scale, axis_order
+
+
+def dense_to_sparse(voxel_data, dtype=np.int):
+    """ From dense representation to sparse (coordinate) representation.
+    No coordinate reordering.
+    """
+    if voxel_data.ndim != 3:
+        raise ValueError('voxel_data is wrong shape; should be 3D array.')
+    return np.asarray(np.nonzero(voxel_data), dtype)
+
+
+def sparse_to_dense(voxel_data, dims, dtype=np.bool):
+    if voxel_data.ndim != 2 or voxel_data.shape[0] != 3:
+        raise ValueError('voxel_data is wrong shape; should be 3xN array.')
+    if np.isscalar(dims):
+        dims = [dims] * 3
+    dims = np.atleast_2d(dims).T
+    # truncate to integers
+    xyz = voxel_data.astype(np.int)
+    # discard voxels that fall outside dims
+    valid_ix = ~np.any((xyz < 0) | (xyz >= dims), 0)
+    xyz = xyz[:, valid_ix]
+    out = np.zeros(dims.flatten(), dtype=dtype)
+    out[tuple(xyz)] = True
+    return out
+
+
+# def get_linear_index(x, y, z, dims):
+# """ Assuming xzy order. (y increasing fastest.
+# TODO ensure this is right when dims are not all same
+# """
+# return x*(dims[1]*dims[2]) + z*dims[1] + y
