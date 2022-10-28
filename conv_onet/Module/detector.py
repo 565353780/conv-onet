f5f76186717c825755a5b4f332b515e082aefd9a
chLi <565353780@qq.com>
2022-10-28 20:19:29 +0800

    add get dataset func, update config path

--- conv_onet/Module/detector.py
+++ conv_onet/Module/detector.py
@@ -19,6 +19,8 @@ from conv_onet.Model.decoder.local_decoder import LocalDecoder
 from conv_onet.Model.decoder.patch_local_decoder import PatchLocalDecoder
 from conv_onet.Model.decoder.local_point_decoder import LocalPointDecoder
 
+from conv_onet.Dataset.shapes3d_dataset import Shapes3dDataset
+
 from conv_onet.Model.encoder.pointnet.local_pool_pointnet import LocalPoolPointnet
 from conv_onet.Model.encoder.pointnet.patch_local_pool_pointnet import PatchLocalPoolPointnet
 from conv_onet.Model.encoder.pointnetpp.pointnet_plus_plus import PointNetPlusPlus
@@ -44,13 +46,6 @@ decoder_dict = {
 
 
 def get_model(cfg, device=None, dataset=None, **kwargs):
-    ''' Return the Occupancy Network model.
-
-    Args:
-        cfg (dict): imported yaml config
-        device (device): pytorch device
-        dataset (dataset): dataset
-    '''
     decoder = cfg['model']['decoder']
     encoder = cfg['model']['encoder']
     dim = cfg['data']['dim']
@@ -114,15 +109,53 @@ def get_model(cfg, device=None, dataset=None, **kwargs):
     return model
 
 
+def get_dataset(mode, cfg, return_idx=False):
+    method = cfg['method']
+    dataset_type = cfg['data']['dataset']
+    dataset_folder = cfg['data']['path']
+    categories = cfg['data']['classes']
+
+    # Get split
+    splits = {
+        'train': cfg['data']['train_split'],
+        'val': cfg['data']['val_split'],
+        'test': cfg['data']['test_split'],
+    }
+
+    split = splits[mode]
+
+    # Create dataset
+    if dataset_type == 'Shapes3D':
+        # Dataset fields
+        # Method specific fields (usually correspond to output)
+        fields = method_dict[method].config.get_data_fields(mode, cfg)
+        # Input fields
+        inputs_field = get_inputs_field(mode, cfg)
+        if inputs_field is not None:
+            fields['inputs'] = inputs_field
+
+        if return_idx:
+            fields['idx'] = data.IndexField()
+
+        dataset = data.Shapes3dDataset(dataset_folder,
+                                       fields,
+                                       split=split,
+                                       categories=categories,
+                                       cfg=cfg)
+    else:
+        raise ValueError('Invalid dataset "%s"' % cfg['data']['dataset'])
+    return dataset
+
+
 class Detector(object):
 
     def __init__(self):
         return
 
     def detect(self):
-        test_config = "configs/pointcloud_crop/demo_matterport.yaml"
+        test_config = "conv_onet/Config/pointcloud_crop/demo_matterport.yaml"
 
-        cfg = config.load_config(test_config, 'configs/default.yaml')
+        cfg = config.load_config(test_config, 'conv_onet/Config/default.yaml')
         device = torch.device("cuda")
 
         out_dir = cfg['training']['out_dir']
