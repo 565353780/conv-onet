412d490b212c8799fb30b6e5c6f3ceaf2b96ee9f
chLi <565353780@qq.com>
2022-10-29 20:34:58 +0800

    remove unused values, move all classes into detector itself

--- conv_onet/Module/detector.py
+++ conv_onet/Module/detector.py
@@ -2,14 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import os
-import time
 import torch
-import shutil
-import pandas as pd
-from tqdm import tqdm
-from collections import defaultdict
-
-from conv_onet.Config.config import load_config
 
 from conv_onet.Data.voxel_grid import VoxelGrid
 from conv_onet.Data.checkpoint_io import CheckpointIO
@@ -18,6 +11,7 @@ from conv_onet.Model.conv_onet import ConvolutionalOccupancyNetwork
 
 from conv_onet.Dataset.shapes3d_dataset import Shapes3dDataset
 
+from conv_onet.Method.config import load_config
 from conv_onet.Method.io import export_pointcloud
 
 from conv_onet.Module.generator3d import Generator3D
@@ -26,210 +20,117 @@ from conv_onet.Module.generator3d import Generator3D
 class Detector(object):
 
     def __init__(self):
+        self.test_config = "conv_onet/Config/pointcloud_crop/demo_matterport.yaml"
+        self.device = torch.device("cuda")
+        self.out_dir = "./output/"
+        self.generation_dir = self.out_dir + "generation/"
+        self.generate_mesh = True
+
+        self.cfg = load_config(self.test_config,
+                               'conv_onet/Config/default.yaml')
+
+        self.input_type = self.cfg['data']['input_type']
+
+        self.dataset = Shapes3dDataset.fromConfig('test', self.cfg, True)
+
+        self.model = ConvolutionalOccupancyNetwork.fromConfig(
+            self.cfg, self.device, self.dataset)
+        self.model.eval()
+
+        self.checkpoint_io = CheckpointIO(self.out_dir, model=self.model)
+        self.checkpoint_io.load(self.cfg['test']['model_file'])
+
+        self.generator = Generator3D.fromConfig(self.model,
+                                                self.cfg,
+                                                device=self.device)
+
+        self.test_loader = torch.utils.data.DataLoader(self.dataset,
+                                                       batch_size=1,
+                                                       num_workers=0,
+                                                       shuffle=False)
+
+        self.time_dicts = []
         return
 
-    def detect(self):
-        test_config = "conv_onet/Config/pointcloud_crop/demo_matterport.yaml"
+    def detect(self, data):
+        mesh_dir = self.generation_dir + 'meshes/'
+        in_dir = self.generation_dir + 'input/'
 
-        cfg = load_config(test_config, 'conv_onet/Config/default.yaml')
-        device = torch.device("cuda")
+        idx = data['idx'].item()
 
-        out_dir = cfg['training']['out_dir']
-        generation_dir = os.path.join(out_dir,
-                                      cfg['generation']['generation_dir'])
-        out_time_file = os.path.join(generation_dir,
-                                     'time_generation_full.pkl')
-        out_time_file_class = os.path.join(generation_dir,
-                                           'time_generation.pkl')
+        model_dict = self.dataset.get_model_dict(idx)
+        #  model_dict = {'model': str(idx), 'category': 'n/a'}
 
-        input_type = cfg['data']['input_type']
-        vis_n_outputs = cfg['generation']['vis_n_outputs']
-        if vis_n_outputs is None:
-            vis_n_outputs = -1
+        modelname = model_dict['model']
+        category_id = model_dict.get('category', 'n/a')
 
-        # Dataset
-        dataset = Shapes3dDataset.fromConfig('test', cfg, True)
+        try:
+            category_name = self.dataset.metadata[category_id].get(
+                'name', 'n/a')
+        except AttributeError:
+            category_name = 'n/a'
 
-        # Model
-        model = ConvolutionalOccupancyNetwork.fromConfig(cfg, device, dataset)
+        if category_id != 'n/a':
+            mesh_dir = os.path.join(mesh_dir, str(category_id))
+            in_dir = os.path.join(in_dir, str(category_id))
 
-        checkpoint_io = CheckpointIO(out_dir, model=model)
-        checkpoint_io.load(cfg['test']['model_file'])
+            folder_name = str(category_id)
+            if category_name != 'n/a':
+                folder_name = str(folder_name) + '_' + category_name.split(
+                    ',')[0]
 
-        # Generator
-        generator = Generator3D.fromConfig(model, cfg, device=device)
+        os.makedirs(mesh_dir, exist_ok=True)
+        os.makedirs(in_dir, exist_ok=True)
 
-        # Determine what to generate
-        generate_mesh = cfg['generation']['generate_mesh']
-        generate_pointcloud = cfg['generation']['generate_pointcloud']
+        # Generate outputs
+        out_file_dict = {}
 
-        if generate_mesh and not hasattr(generator, 'generate_mesh'):
-            generate_mesh = False
-            print('Warning: generator does not support mesh generation.')
+        # Also copy ground truth
+        if self.cfg['generation']['copy_groundtruth']:
+            modelpath = os.path.join(self.dataset.dataset_folder, category_id,
+                                     modelname,
+                                     self.cfg['data']['watertight_file'])
+            out_file_dict['gt'] = modelpath
 
-        if generate_pointcloud and not hasattr(generator,
-                                               'generate_pointcloud'):
-            generate_pointcloud = False
-            print('Warning: generator does not support pointcloud generation.')
-
-        # Loader
-        test_loader = torch.utils.data.DataLoader(dataset,
-                                                  batch_size=1,
-                                                  num_workers=0,
-                                                  shuffle=False)
-
-        # Statistics
-        time_dicts = []
-
-        # Generate
-        model.eval()
-
-        # Count how many models already created
-        model_counter = defaultdict(int)
-
-        for it, data in enumerate(tqdm(test_loader)):
-            # Output folders
-            mesh_dir = os.path.join(generation_dir, 'meshes')
-            pointcloud_dir = os.path.join(generation_dir, 'pointcloud')
-            in_dir = os.path.join(generation_dir, 'input')
-            generation_vis_dir = os.path.join(generation_dir, 'vis')
-
-            # Get index etc.
-            idx = data['idx'].item()
+        if self.generate_mesh:
+            if self.cfg['generation']['sliding_window']:
+                out = self.generator.generate_mesh_sliding(data)
+            else:
+                out = self.generator.generate_mesh(data)
 
             try:
-                model_dict = dataset.get_model_dict(idx)
-            except AttributeError:
-                model_dict = {'model': str(idx), 'category': 'n/a'}
+                mesh, stats_dict = out
+            except TypeError:
+                mesh, stats_dict = out, {}
 
-            modelname = model_dict['model']
-            category_id = model_dict.get('category', 'n/a')
+            mesh_out_file = os.path.join(mesh_dir, '%s.off' % modelname)
+            mesh.export(mesh_out_file)
+            out_file_dict['mesh'] = mesh_out_file
 
-            try:
-                category_name = dataset.metadata[category_id].get(
-                    'name', 'n/a')
-            except AttributeError:
-                category_name = 'n/a'
+        if self.cfg['generation']['copy_input']:
+            # Save inputs
+            if self.input_type == 'voxels':
+                inputs_path = os.path.join(in_dir, '%s.off' % modelname)
+                inputs = data['inputs'].squeeze(0).cpu()
+                voxel_mesh = VoxelGrid(inputs).to_mesh()
+                voxel_mesh.export(inputs_path)
+                out_file_dict['in'] = inputs_path
+            elif self.input_type == 'pointcloud_crop':
+                inputs_path = os.path.join(in_dir, '%s.ply' % modelname)
+                inputs = data['inputs'].squeeze(0).cpu().numpy()
+                export_pointcloud(inputs, inputs_path, False)
+                out_file_dict['in'] = inputs_path
+            elif self.input_type == 'pointcloud' or 'partial_pointcloud':
+                inputs_path = os.path.join(in_dir, '%s.ply' % modelname)
+                inputs = data['inputs'].squeeze(0).cpu().numpy()
+                export_pointcloud(inputs, inputs_path, False)
+                out_file_dict['in'] = inputs_path
+        return True
 
-            if category_id != 'n/a':
-                mesh_dir = os.path.join(mesh_dir, str(category_id))
-                pointcloud_dir = os.path.join(pointcloud_dir, str(category_id))
-                in_dir = os.path.join(in_dir, str(category_id))
-
-                folder_name = str(category_id)
-                if category_name != 'n/a':
-                    folder_name = str(folder_name) + '_' + category_name.split(
-                        ',')[0]
-
-                generation_vis_dir = os.path.join(generation_vis_dir,
-                                                  folder_name)
-
-            # Create directories if necessary
-            if vis_n_outputs >= 0 and not os.path.exists(generation_vis_dir):
-                os.makedirs(generation_vis_dir)
-
-            if generate_mesh and not os.path.exists(mesh_dir):
-                os.makedirs(mesh_dir)
-
-            if generate_pointcloud and not os.path.exists(pointcloud_dir):
-                os.makedirs(pointcloud_dir)
-
-            if not os.path.exists(in_dir):
-                os.makedirs(in_dir)
-
-            # Timing dict
-            time_dict = {
-                'idx': idx,
-                'class id': category_id,
-                'class name': category_name,
-                'modelname': modelname,
-            }
-            time_dicts.append(time_dict)
-
-            # Generate outputs
-            out_file_dict = {}
-
-            # Also copy ground truth
-            if cfg['generation']['copy_groundtruth']:
-                modelpath = os.path.join(dataset.dataset_folder, category_id,
-                                         modelname,
-                                         cfg['data']['watertight_file'])
-                out_file_dict['gt'] = modelpath
-
-            if generate_mesh:
-                t0 = time.time()
-                if cfg['generation']['sliding_window']:
-                    if it == 0:
-                        print('Process scenes in a sliding-window manner')
-                    out = generator.generate_mesh_sliding(data)
-                else:
-                    out = generator.generate_mesh(data)
-                time_dict['mesh'] = time.time() - t0
-
-                # Get statistics
-                try:
-                    mesh, stats_dict = out
-                except TypeError:
-                    mesh, stats_dict = out, {}
-                time_dict.update(stats_dict)
-
-                # Write output
-                mesh_out_file = os.path.join(mesh_dir, '%s.off' % modelname)
-                mesh.export(mesh_out_file)
-                out_file_dict['mesh'] = mesh_out_file
-
-            if generate_pointcloud:
-                t0 = time.time()
-                pointcloud = generator.generate_pointcloud(data)
-                time_dict['pcl'] = time.time() - t0
-                pointcloud_out_file = os.path.join(pointcloud_dir,
-                                                   '%s.ply' % modelname)
-                export_pointcloud(pointcloud, pointcloud_out_file)
-                out_file_dict['pointcloud'] = pointcloud_out_file
-
-            if cfg['generation']['copy_input']:
-                # Save inputs
-                if input_type == 'voxels':
-                    inputs_path = os.path.join(in_dir, '%s.off' % modelname)
-                    inputs = data['inputs'].squeeze(0).cpu()
-                    voxel_mesh = VoxelGrid(inputs).to_mesh()
-                    voxel_mesh.export(inputs_path)
-                    out_file_dict['in'] = inputs_path
-                elif input_type == 'pointcloud_crop':
-                    inputs_path = os.path.join(in_dir, '%s.ply' % modelname)
-                    inputs = data['inputs'].squeeze(0).cpu().numpy()
-                    export_pointcloud(inputs, inputs_path, False)
-                    out_file_dict['in'] = inputs_path
-                elif input_type == 'pointcloud' or 'partial_pointcloud':
-                    inputs_path = os.path.join(in_dir, '%s.ply' % modelname)
-                    inputs = data['inputs'].squeeze(0).cpu().numpy()
-                    export_pointcloud(inputs, inputs_path, False)
-                    out_file_dict['in'] = inputs_path
-
-            # Copy to visualization directory for first vis_n_output samples
-            c_it = model_counter[category_id]
-            if c_it < vis_n_outputs:
-                # Save output files
-                img_name = '%02d.off' % c_it
-                for k, filepath in out_file_dict.items():
-                    ext = os.path.splitext(filepath)[1]
-                    out_file = os.path.join(generation_vis_dir,
-                                            '%02d_%s%s' % (c_it, k, ext))
-                    shutil.copyfile(filepath, out_file)
-
-            model_counter[category_id] += 1
-
-        # Create pandas dataframe and save
-        time_df = pd.DataFrame(time_dicts)
-        time_df.set_index(['idx'], inplace=True)
-        time_df.to_pickle(out_time_file)
-
-        # Create pickle files  with main statistics
-        time_df_class = time_df.groupby(by=['class name']).mean()
-        time_df_class.to_pickle(out_time_file_class)
-
-        # Print results
-        time_df_class.loc['mean'] = time_df_class.mean()
-        print('Timings [s]:')
-        print(time_df_class)
+    def detectAll(self):
+        for i, data in enumerate(self.test_loader):
+            print("[INFO][Detector::detectAll]")
+            print("\t start detect data " + str(i) + "...")
+            self.detect(data)
+            break
         return True
