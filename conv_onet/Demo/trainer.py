9ecddeac50bdf81bf4a40d2b92d9be00ab0478d3
chLi <565353780@qq.com>
2022-10-30 02:02:19 +0800

    refine format

--- conv_onet/Demo/trainer.py
+++ conv_onet/Demo/trainer.py
@@ -25,13 +25,17 @@ def demo():
 
     # Arguments
     parser = argparse.ArgumentParser(
-        description='Train a 3D reconstruction model.'
-    )
+        description='Train a 3D reconstruction model.')
     parser.add_argument('config', type=str, help='Path to config file.')
-    parser.add_argument('--no-cuda', action='store_true', help='Do not use cuda.')
-    parser.add_argument('--exit-after', type=int, default=-1,
-                        help='Checkpoint and exit after specified number of seconds'
-                             'with exit code 2.')
+    parser.add_argument('--no-cuda',
+                        action='store_true',
+                        help='Do not use cuda.')
+    parser.add_argument(
+        '--exit-after',
+        type=int,
+        default=-1,
+        help='Checkpoint and exit after specified number of seconds'
+        'with exit code 2.')
 
     args = parser.parse_args()
     cfg = config.load_config(args.config, 'configs/default.yaml')
@@ -67,18 +71,26 @@ def demo():
     val_dataset = config.get_dataset('val', cfg, return_idx=True)
 
     train_loader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=batch_size, num_workers=cfg['training']['n_workers'], shuffle=True,
+        train_dataset,
+        batch_size=batch_size,
+        num_workers=cfg['training']['n_workers'],
+        shuffle=True,
         collate_fn=data.collate_remove_none,
         worker_init_fn=data.worker_init_fn)
 
     val_loader = torch.utils.data.DataLoader(
-            val_dataset, batch_size=1, num_workers=cfg['training']['n_workers_val'], shuffle=False,
+        val_dataset,
+        batch_size=1,
+        num_workers=cfg['training']['n_workers_val'],
+        shuffle=False,
         collate_fn=data.collate_remove_none,
         worker_init_fn=data.worker_init_fn)
 
     # For visualizations
     vis_loader = torch.utils.data.DataLoader(
-        val_dataset, batch_size=1, shuffle=False,
+        val_dataset,
+        batch_size=1,
+        shuffle=False,
         collate_fn=data.collate_remove_none,
         worker_init_fn=data.worker_init_fn)
     model_counter = defaultdict(int)
@@ -98,7 +110,11 @@ def demo():
 
         c_it = model_counter[category_id]
         if c_it < vis_n_outputs:
-            data_vis_list.append({'category': category_name, 'it': c_it, 'data': data_vis})
+            data_vis_list.append({
+                'category': category_name,
+                'it': c_it,
+                'data': data_vis
+            })
 
         model_counter[category_id] += 1
 
@@ -120,13 +136,13 @@ def demo():
         load_dict = dict()
     epoch_it = load_dict.get('epoch_it', 0)
     it = load_dict.get('it', 0)
-    metric_val_best = load_dict.get(
-        'loss_val_best', -model_selection_sign * np.inf)
+    metric_val_best = load_dict.get('loss_val_best',
+                                    -model_selection_sign * np.inf)
 
     if metric_val_best == np.inf or metric_val_best == -np.inf:
         metric_val_best = -model_selection_sign * np.inf
-    print('Current best validation metric (%s): %.8f'
-          % (model_selection_metric, metric_val_best))
+    print('Current best validation metric (%s): %.8f' %
+          (model_selection_metric, metric_val_best))
     logger = SummaryWriter(os.path.join(out_dir, 'logs'))
 
     # Shorthands
@@ -152,43 +168,44 @@ def demo():
             # Print output
             if print_every > 0 and (it % print_every) == 0:
                 t = datetime.datetime.now()
-                print('[Epoch %02d] it=%03d, loss=%.4f, time: %.2fs, %02d:%02d'
-                         % (epoch_it, it, loss, time.time() - t0, t.hour, t.minute))
+                print(
+                    '[Epoch %02d] it=%03d, loss=%.4f, time: %.2fs, %02d:%02d' %
+                    (epoch_it, it, loss, time.time() - t0, t.hour, t.minute))
 
             # Visualize output
             if visualize_every > 0 and (it % visualize_every) == 0:
                 print('Visualizing')
                 for data_vis in data_vis_list:
-                    if cfg['generation']['sliding_window']:
-                        out = generator.generate_mesh_sliding(data_vis['data'])    
-                    else:
-                        out = generator.generate_mesh(data_vis['data'])
-                    # Get statistics
-                    try:
-                        mesh, stats_dict = out
-                    except TypeError:
-                        mesh, stats_dict = out, {}
+                    mesh, stats_dict = generator.generate_mesh_sliding(
+                        data_vis['data'])
 
-                    mesh.export(os.path.join(out_dir, 'vis', '{}_{}_{}.off'.format(it, data_vis['category'], data_vis['it'])))
-
+                    mesh.export(
+                        os.path.join(
+                            out_dir, 'vis',
+                            '{}_{}_{}.off'.format(it, data_vis['category'],
+                                                  data_vis['it'])))
 
             # Save checkpoint
             if (checkpoint_every > 0 and (it % checkpoint_every) == 0):
                 print('Saving checkpoint')
-                checkpoint_io.save('model.pt', epoch_it=epoch_it, it=it,
+                checkpoint_io.save('model.pt',
+                                   epoch_it=epoch_it,
+                                   it=it,
                                    loss_val_best=metric_val_best)
 
             # Backup if necessary
             if (backup_every > 0 and (it % backup_every) == 0):
                 print('Backup checkpoint')
-                checkpoint_io.save('model_%d.pt' % it, epoch_it=epoch_it, it=it,
+                checkpoint_io.save('model_%d.pt' % it,
+                                   epoch_it=epoch_it,
+                                   it=it,
                                    loss_val_best=metric_val_best)
             # Run validation
             if validate_every > 0 and (it % validate_every) == 0:
                 eval_dict = trainer.evaluate(val_loader)
                 metric_val = eval_dict[model_selection_metric]
-                print('Validation metric (%s): %.4f'
-                      % (model_selection_metric, metric_val))
+                print('Validation metric (%s): %.4f' %
+                      (model_selection_metric, metric_val))
 
                 for k, v in eval_dict.items():
                     logger.add_scalar('val/%s' % k, v, it)
@@ -196,13 +213,17 @@ def demo():
                 if model_selection_sign * (metric_val - metric_val_best) > 0:
                     metric_val_best = metric_val
                     print('New best model (loss %.4f)' % metric_val_best)
-                    checkpoint_io.save('model_best.pt', epoch_it=epoch_it, it=it,
+                    checkpoint_io.save('model_best.pt',
+                                       epoch_it=epoch_it,
+                                       it=it,
                                        loss_val_best=metric_val_best)
 
             # Exit if necessary
             if exit_after > 0 and (time.time() - t0) >= exit_after:
                 print('Time limit reached. Exiting.')
-                checkpoint_io.save('model.pt', epoch_it=epoch_it, it=it,
+                checkpoint_io.save('model.pt',
+                                   epoch_it=epoch_it,
+                                   it=it,
                                    loss_val_best=metric_val_best)
                 exit(3)
     return True
