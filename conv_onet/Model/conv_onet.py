44162e9490d92b23fb6d63b3280bf596f1160ea9
chLi <565353780@qq.com>
2022-10-29 19:18:46 +0800

    add fromConfig call

--- conv_onet/Model/conv_onet.py
+++ conv_onet/Model/conv_onet.py
@@ -5,6 +5,31 @@ import torch
 import torch.nn as nn
 from torch import distributions as dist
 
+from conv_onet.Model.decoder.local_decoder import LocalDecoder
+from conv_onet.Model.decoder.patch_local_decoder import PatchLocalDecoder
+from conv_onet.Model.decoder.local_point_decoder import LocalPointDecoder
+
+from conv_onet.Model.encoder.pointnet.local_pool_pointnet import LocalPoolPointnet
+from conv_onet.Model.encoder.pointnet.patch_local_pool_pointnet import PatchLocalPoolPointnet
+from conv_onet.Model.encoder.pointnetpp.pointnet_plus_plus import PointNetPlusPlus
+from conv_onet.Model.encoder.voxel.local_voxel_encoder import LocalVoxelEncoder
+
+from conv_onet.Method.common import update_reso
+
+encoder_dict = {
+    'pointnet_local_pool': LocalPoolPointnet,
+    'pointnet_crop_local_pool': PatchLocalPoolPointnet,
+    'pointnet_plus_plus': PointNetPlusPlus,
+    'voxel_simple_local': LocalVoxelEncoder,
+}
+
+# Decoder dictionary
+decoder_dict = {
+    'simple_local': LocalDecoder,
+    'simple_local_crop': PatchLocalDecoder,
+    'simple_local_point': LocalPointDecoder
+}
+
 
 class ConvolutionalOccupancyNetwork(nn.Module):
 
@@ -19,6 +44,70 @@ class ConvolutionalOccupancyNetwork(nn.Module):
             self.encoder = None
 
         self._device = device
+
+    @classmethod
+    def fromConfig(cls, cfg, device=None, dataset=None, **kwargs):
+        encoder = cfg['model']['encoder']
+        decoder = cfg['model']['decoder']
+
+        dim = cfg['data']['dim']
+        c_dim = cfg['model']['c_dim']
+
+        encoder_kwargs = cfg['model']['encoder_kwargs']
+        decoder_kwargs = cfg['model']['decoder_kwargs']
+
+        padding = cfg['data']['padding']
+
+        # for pointcloud_crop
+        try:
+            encoder_kwargs['unit_size'] = cfg['data']['unit_size']
+            decoder_kwargs['unit_size'] = cfg['data']['unit_size']
+        except:
+            pass
+        # local positional encoding
+        if 'local_coord' in cfg['model'].keys():
+            encoder_kwargs['local_coord'] = cfg['model']['local_coord']
+            decoder_kwargs['local_coord'] = cfg['model']['local_coord']
+        if 'pos_encoding' in cfg['model']:
+            encoder_kwargs['pos_encoding'] = cfg['model']['pos_encoding']
+            decoder_kwargs['pos_encoding'] = cfg['model']['pos_encoding']
+
+        # update the feature volume/plane resolution
+        if cfg['data']['input_type'] == 'pointcloud_crop':
+            fea_type = cfg['model']['encoder_kwargs']['plane_type']
+            if (dataset.split
+                    == 'train') or (cfg['generation']['sliding_window']):
+                recep_field = 2**(cfg['model']['encoder_kwargs']
+                                  ['unet3d_kwargs']['num_levels'] + 2)
+                reso = cfg['data']['query_vol_size'] + recep_field - 1
+                if 'grid' in fea_type:
+                    encoder_kwargs['grid_resolution'] = update_reso(
+                        reso, dataset.depth)
+                if bool(set(fea_type) & set(['xz', 'xy', 'yz'])):
+                    encoder_kwargs['plane_resolution'] = update_reso(
+                        reso, dataset.depth)
+            # if dataset.split == 'val': #TODO run validation in room level during training
+            else:
+                if 'grid' in fea_type:
+                    encoder_kwargs['grid_resolution'] = dataset.total_reso
+                if bool(set(fea_type) & set(['xz', 'xy', 'yz'])):
+                    encoder_kwargs['plane_resolution'] = dataset.total_reso
+
+        decoder = decoder_dict[decoder](dim=dim,
+                                        c_dim=c_dim,
+                                        padding=padding,
+                                        **decoder_kwargs)
+
+        if encoder == 'idx':
+            encoder = nn.Embedding(len(dataset), c_dim)
+        elif encoder is not None:
+            encoder = encoder_dict[encoder](dim=dim,
+                                            c_dim=c_dim,
+                                            padding=padding,
+                                            **encoder_kwargs)
+        else:
+            encoder = None
+        return cls(decoder, encoder, device=device)
 
     def forward(self, p, inputs, sample=True, **kwargs):
         c = self.encode_inputs(inputs)
