a27bbb278b1124c5a8296c8da38e131c6b3514bd
chLi <565353780@qq.com>
2022-10-30 01:39:45 +0800

    remove get_data_fields for pointcloud_crop condition

--- conv_onet/Data/field/fields.py
+++ conv_onet/Data/field/fields.py
@@ -3,47 +3,13 @@
 
 from torchvision import transforms
 
-from conv_onet.Data.field.index_field import IndexField
-from conv_onet.Data.field.partial_point_cloud_field import PartialPointCloudField
 from conv_onet.Data.field.patch_point_cloud_field import PatchPointCloudField
 from conv_onet.Data.field.patch_points_field import PatchPointsField
-from conv_onet.Data.field.point_cloud_field import PointCloudField
-from conv_onet.Data.field.points_field import PointsField
 from conv_onet.Data.field.voxels_field import VoxelsField
 
 from conv_onet.Data.transform.pointcloud_noise import PointcloudNoise
 from conv_onet.Data.transform.subsample_pointcloud import SubsamplePointcloud
 from conv_onet.Data.transform.subsample_points import SubsamplePoints
-
-
-def get_data_fields(mode, cfg):
-    ''' Returns the data fields.
-
-    Args:
-        mode (str): the mode which is used
-        cfg (dict): imported yaml config
-    '''
-    points_transform = SubsamplePoints(cfg['data']['points_subsample'])
-
-    fields = {}
-    if cfg['data']['points_file'] is not None:
-        fields['points'] = PatchPointsField(
-            cfg['data']['points_file'],
-            transform=points_transform,
-            unpackbits=cfg['data']['points_unpackbits'],
-            multi_files=cfg['data']['multi_files'])
-
-    if mode in ('val', 'test'):
-        points_iou_file = cfg['data']['points_iou_file']
-        voxels_file = cfg['data']['voxels_file']
-        if points_iou_file is not None:
-            fields['points_iou'] = PatchPointsField(
-                points_iou_file,
-                unpackbits=cfg['data']['points_unpackbits'],
-                multi_files=cfg['data']['multi_files'])
-        if voxels_file is not None:
-            fields['voxels'] = VoxelsField(voxels_file)
-    return fields
 
 
 def get_inputs_field(mode, cfg):
