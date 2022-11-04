4d0a17e6e0d0fbb9531308472cf8df9595fb3091
chLi <565353780@qq.com>
2022-11-04 21:04:22 +0800

    add test mesh file path and preprocess algo

--- conv_onet/Demo/detector.py
+++ conv_onet/Demo/detector.py
@@ -8,11 +8,24 @@ from conv_onet.Module.detector import Detector
 
 
 def demo():
-    pcd_file_path = \
-        "/home/chli/chLi/ScanNet/objects/scene0474_02/2_chair.ply"
 
-    pcd = o3d.io.read_point_cloud(pcd_file_path)
+    #  mesh_file_path = \
+        #  "/home/chli/chLi/ScanNet/objects/scene0000_00/37_bed.ply"
+    #  mesh = o3d.io.read_triangle_mesh(mesh_file_path)
+    #  points = np.array(mesh.vertices)
+
+    mesh_file_path = \
+        "/home/chli/chLi/ShapeNet/Core/ShapeNetCore.v2/" + \
+        "02691156/" + \
+        "222c0d99446148babe4274edc10c1c8e/" + \
+        "models/model_normalized.obj"
+    mesh = o3d.io.read_triangle_mesh(mesh_file_path)
+    pcd = mesh.sample_points_uniformly(2000000)
     points = np.array(pcd.points)
+    box = pcd.get_axis_aligned_bounding_box()
+    print(box)
+    print(box.get_extent())
+
     point_idx_array = np.arange(0, points.shape[0])
     sample_point_idx_array = np.random.choice(point_idx_array,
                                               int(points.shape[0] / 2),
