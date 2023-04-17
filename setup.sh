3269c41df65f3ea8d0ecf8eb5b484a1f6b5dcfda
chLi <565353780@qq.com>
2023-04-17 22:50:48 +0800

    update torch and cuda version

--- setup.sh
+++ setup.sh
@@ -1,11 +1,10 @@
 pip install cython imageio numpy pandas pillow pyembree pytest pyyaml \
   scikit-image scipy tensorboardx tqdm trimesh h5py plyfile open3d
 
-pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
+pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu118
 
-pip install torch-scatter -f https://data.pyg.org/whl/torch-1.12.0+cu113.html
+pip install torch-scatter -f https://data.pyg.org/whl/torch-2.0.0+cu118.html
 
 python setup.py build_ext --inplace
 
 CC=gcc CXX=gcc python cc_setup.py build_ext --inplace
-
