3471d5c993474fcc43259c6aa36c4d7d6d55e610
chLi <565353780@qq.com>
2023-04-20 22:28:19 +0800

    downgrade cuda version

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,10 +1,10 @@
 pip install cython imageio numpy pandas pillow pyembree pytest pyyaml \
   scikit-image scipy tensorboardx tqdm trimesh h5py plyfile open3d
 
-pip install torch==1.13.1+cu117 torchvision==0.14.1+cu117 torchaudio==0.13.1 \
-  --extra-index-url https://download.pytorch.org/whl/cu117
+pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 torchaudio==0.13.1 \
+  --extra-index-url https://download.pytorch.org/whl/cu116
 
-pip install torch-scatter -f https://data.pyg.org/whl/torch-1.13.1+cu117.html
+pip install torch-scatter -f https://data.pyg.org/whl/torch-1.13.1+cu116.html
 
 python setup.py build_ext --inplace
 
