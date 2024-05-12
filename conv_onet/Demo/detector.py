7978283957ad94d4d063b9c977e8332305413a69
chLi <565353780@qq.com>
2024-05-12 14:14:13 +0800

    create a simple clean detector demo script

--- conv_onet/Demo/detector.py
+++ conv_onet/Demo/detector.py
@@ -1,4 +1,3 @@
-import os
 import numpy as np
 import open3d as o3d
 
@@ -6,41 +5,21 @@ from conv_onet.Module.detector import Detector
 
 
 def demo():
-    dataset_folder_path = '/home/chli/chLi/Dataset/SampledPcd_Manifold/ShapeNet/03001627/'
+    pcd_file_path = '/home/chli/chLi/Dataset/SampledPcd_Manifold/ShapeNet/03001627/46bd3baefe788d166c05d60b45815.npy'
     sample_point_num = 4000
-    save_folder_path = '/home/chli/chLi/Dataset/ConvONet_Manifold_Recon_' + str(sample_point_num) + '/ShapeNet/03001627/'
+    save_mesh_file_path = './output/test.obj'
     render = False
     print_progress = True
 
-    os.makedirs(save_folder_path, exist_ok=True)
-
     detector = Detector()
 
-    solved_shape_names = os.listdir(save_folder_path)
+    points = np.load(pcd_file_path)
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(points)
 
-    pcd_filename_list = os.listdir(dataset_folder_path)
-    pcd_filename_list.sort()
+    fps_pcd = pcd.farthest_point_down_sample(sample_point_num)
 
-    for i, pcd_filename in enumerate(pcd_filename_list):
-        if pcd_filename[-4:] != '.npy':
-            continue
+    fps_pts = np.asarray(fps_pcd.points).reshape(1, -1, 3)
 
-        if pcd_filename in solved_shape_names:
-            continue
-
-        pcd_file_path = dataset_folder_path + pcd_filename
-
-        points = np.load(pcd_file_path)
-        pcd = o3d.geometry.PointCloud()
-        pcd.points = o3d.utility.Vector3dVector(points)
-
-        fps_pcd = pcd.farthest_point_down_sample(sample_point_num)
-
-        fps_pts = np.asarray(fps_pcd.points).reshape(1, -1, 3)
-
-        save_mesh_file_path = save_folder_path + pcd_filename.replace('.npy', '.obj')
-
-        detector.detectAndSave(fps_pts, save_mesh_file_path, render, print_progress)
-
-        print('solved shape num:', i + 1)
+    detector.detectAndSave(fps_pts, save_mesh_file_path, render, print_progress)
     return True
