d0f3325ea5132f0ff2a380132427954a42aab1e6
chLi <565353780@qq.com>
2024-05-16 19:39:08 +0800

    make auto convert mash with category-first order

--- convert_mash.py
+++ convert_mash.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import open3d as o3d
+from tqdm import tqdm
 from time import sleep
 
 from conv_onet.Module.detector import Detector
@@ -9,43 +10,72 @@ from conv_onet.Module.detector import Detector
 def demo():
     print('start convert new data...')
 
+    dataset_folder_path = '/home/chli/chLi/Dataset/SampledPcd_Manifold/ShapeNet/'
+    check_folder_path = '/home/chli/chLi/Dataset/MashV4/ShapeNet/'
+    sample_point_num = 4000
+    save_folder_path = '/home/chli/chLi/Dataset/ConvONet_Manifold_Recon_' + str(sample_point_num) + '/ShapeNet/'
+    render = False
+    print_progress = True
+
     detector = Detector()
 
-    first_solve_list = ['03001627', '02691156']
-    for category_id in first_solve_list:
-        compare_folder_path = '../ma-sh/output/metric_manifold_result_selected/ShapeNet/' + category_id + '/'
+    classname_list = os.listdir(dataset_folder_path)
+    classname_list.sort()
 
-        first_solve_shape_ids = None
-        if os.path.exists(compare_folder_path):
-            first_solve_shape_ids = os.listdir(compare_folder_path)
-        #first_solve_shape_ids = None
+    model_filename_list_dict = {}
+    check_model_filename_list_dict = {}
+    solved_model_filename_list_dict = {}
+    max_shape_num = 0
 
-        dataset_folder_path = '/home/chli/chLi/Dataset/SampledPcd_Manifold/ShapeNet/' + category_id + '/'
-        sample_point_num = 4000
-        save_folder_path = '/home/chli/chLi/Dataset/ConvONet_Manifold_Recon_' + str(sample_point_num) + '/ShapeNet/' + category_id + '/'
-        render = False
-        print_progress = True
-        os.makedirs(save_folder_path, exist_ok=True)
+    for classname in tqdm(classname_list):
+        class_folder_path = dataset_folder_path + classname + '/'
 
-        solved_shape_names = os.listdir(save_folder_path)
+        model_filename_list = os.listdir(class_folder_path)
+        model_filename_list.sort()
 
-        pcd_filename_list = os.listdir(dataset_folder_path)
-        pcd_filename_list.sort()
+        max_shape_num = max(max_shape_num, len(model_filename_list))
+        model_filename_list_dict[classname] = model_filename_list
 
-        for i, pcd_filename in enumerate(pcd_filename_list):
-            if pcd_filename[-4:] != '.npy':
+        check_model_filename_list_dict[classname] = []
+
+        class_check_folder_path = check_folder_path + classname + '/'
+        if not os.path.exists(class_check_folder_path):
+            continue
+
+        check_model_filename_list = os.listdir(class_check_folder_path)
+        check_model_filename_list.sort()
+        check_model_filename_list_dict[classname] = check_model_filename_list
+
+        solved_model_filename_list_dict[classname] = []
+        class_save_folder_path = save_folder_path + classname + "/"
+        os.makedirs(class_save_folder_path, exist_ok=True)
+        solved_model_filename_list = os.listdir(class_save_folder_path)
+        solved_model_filename_list.sort()
+        solved_model_filename_list_dict[classname] = solved_model_filename_list
+
+    solved_shape_num = 0
+    for i in range(max_shape_num):
+        for classname, model_filename_list in model_filename_list_dict.items():
+            if len(model_filename_list) <= i:
                 continue
 
-            if first_solve_shape_ids is not None:
-                if pcd_filename.split('.npy')[0] not in first_solve_shape_ids:
-                    continue
+            model_filename = model_filename_list[i]
 
-            if pcd_filename.replace('.npy', '.obj') in solved_shape_names:
+            if model_filename[-4:] != '.npy':
                 continue
 
-            print('start convert:', pcd_filename)
+            check_model_filename_list = check_model_filename_list_dict[classname]
+            if model_filename not in check_model_filename_list:
+                continue
 
-            pcd_file_path = dataset_folder_path + pcd_filename
+            solved_model_filename_list = solved_model_filename_list_dict[classname]
+            if model_filename.replace('.npy', '.obj') in solved_model_filename_list:
+                continue
+
+            class_folder_path = dataset_folder_path + classname + '/'
+            class_save_folder_path = save_folder_path + classname + "/"
+
+            pcd_file_path = class_folder_path + model_filename
 
             points = np.load(pcd_file_path)
             pcd = o3d.geometry.PointCloud()
@@ -55,11 +85,13 @@ def demo():
 
             fps_pts = np.asarray(fps_pcd.points).reshape(1, -1, 3)
 
-            save_mesh_file_path = save_folder_path + pcd_filename.replace('.npy', '.obj')
+            save_mesh_file_path = class_save_folder_path + model_filename.replace('.npy', '.obj')
 
             detector.detectAndSave(fps_pts, save_mesh_file_path, render, print_progress)
 
-            print('category:', category_id, 'solved shape num:', i + 1)
+            solved_shape_num += 1
+
+            print('category:', classname, 'solved shape num:', solved_shape_num)
 
     print('convert new data finished!')
     return True
